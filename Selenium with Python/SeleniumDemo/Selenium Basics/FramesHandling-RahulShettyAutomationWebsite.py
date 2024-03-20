6b7b3a2e67ce1f38afdb9b75a8223fd2f7faf3b3
Haryish <haryishkumaran16@outlook.com>
2024-03-20 23:26:34 +0530

    Commit 16: Frame Handling concept Accomplished

--- Selenium with Python/SeleniumDemo/Selenium Basics/FramesHandling-RahulShettyAutomationWebsite.py
+++ Selenium with Python/SeleniumDemo/Selenium Basics/FramesHandling-RahulShettyAutomationWebsite.py
@@ -1,20 +1,28 @@
-import time
-
 from selenium import webdriver
 # from selenium.common import WebDriverException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-if __name__ == '__main__':
-    # Chrome Drive - ChromeBrowser
-    serviceObj = Service()  # selenium webservice Manager / seleniumManager
-    driver = webdriver.Chrome(service=serviceObj)
+# Chrome Drive - ChromeBrowser
+serviceObj = Service()  # selenium webservice Manager / seleniumManager
+driver = webdriver.Chrome(service=serviceObj)
 
-    sourceUrl = 'https://rahulshettyacademy.com/loginpagePractise/'
+url1 = "https://rahulshettyacademy.com/AutomationPractice/"
 
-    driver.maximize_window()
-    driver.get(sourceUrl)
+driver.maximize_window()
 
-    driver.implicitly_wait(5)
+driver.get(url1)
 
-    driver.close()
+driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+
+driver.switch_to.frame("courses-iframe")
+
+getText = driver.find_element(By.XPATH, "//div[@class='col-md-6 text-left']//h2").text
+
+print(getText)
+
+driver.switch_to.default_content()
+
+driver.execute_script("window.scrollBy(document.body.scrollHeight,0)")
+
+driver.quit()
