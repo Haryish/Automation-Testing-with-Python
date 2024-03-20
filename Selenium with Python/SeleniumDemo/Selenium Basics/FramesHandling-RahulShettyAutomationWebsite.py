65a274a1e2c22d1e4847069c7030e0279a2e94b7
Haryish <haryishkumaran16@outlook.com>
2024-03-21 00:03:11 +0530

    Commit 17: Scrolling using JS Executor Accomplished

--- Selenium with Python/SeleniumDemo/Selenium Basics/FramesHandling-RahulShettyAutomationWebsite.py
+++ Selenium with Python/SeleniumDemo/Selenium Basics/FramesHandling-RahulShettyAutomationWebsite.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 # from selenium.common import WebDriverException
 from selenium.webdriver.chrome.service import Service
@@ -13,7 +15,11 @@ driver.maximize_window()
 
 driver.get(url1)
 
-driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+frame = driver.find_element(By.XPATH, "//body/div[@class='block large-row-spacer']/fieldset[1]")
+
+driver.execute_script("arguments[0].scrollIntoView(true)", frame)
+
+# driver.execute_script(window.scrollTo(0,document.body.scrollHeight))
 
 driver.switch_to.frame("courses-iframe")
 
@@ -23,6 +29,10 @@ print(getText)
 
 driver.switch_to.default_content()
 
-driver.execute_script("window.scrollBy(document.body.scrollHeight,0)")
+time.sleep(5)
+
+driver.execute_script("window.scrollTo({behavior: 'instant', top: 0, left: 0})")
+
+time.sleep(5)
 
 driver.quit()
